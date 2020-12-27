8f1d1fc9e867a1e1406f435f5c2c64c7a479a937
hblee0619 <hblee0619@gmail.com>
2020-12-28 07:42:36 +0900

    updated - movie_app

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <span>
+      About this page: I built it because I'm trying learning programming
+    </span>
+  );
+}
+
+export default About;
