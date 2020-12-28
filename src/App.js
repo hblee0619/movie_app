923bf5ad4958f4a155ec5ac4341671e9a0568f01
hblee0619 <hblee0619@gmail.com>
2020-12-29 02:53:55 +0900

    updated - movie_app / router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
-  return <span>something </span>;
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
